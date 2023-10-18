7514a31ef00c799dd72b2c5d8a4041d4256b0694
Bassel El-Sheikh <bassel.elsheikh@gmail.com>
2023-10-18 14:58:41 +0200

    Added Stores Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,160 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)),
+        home: DefaultTabController(
+            length: 4,
+            child: Scaffold(
+              appBar: AppBar(
+                title: const Text("eCommerce"),
+                bottom: const TabBar(tabs: [
+                  Tab(
+                    icon: Icon(Icons.store),
+                    text: "Stores",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.smartphone),
+                    text: "Products",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.shopping_cart),
+                    text: "My Cart",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.receipt_long),
+                    text: "My Orders",
+                  ),
+                ]),
+              ),
+              body: const TabBarView(
+                children: [
+                  StoresHomePage(),
+                  Placeholder(),
+                  Placeholder(),
+                  Placeholder()
+                ],
+              ),
+            )));
+  }
+}
+
+class StoreCard extends StatelessWidget {
+  const StoreCard({super.key, required this.store});
+  final Store store;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => StorePage(
+                      store: store,
+                    )));
+      },
+      child: Card(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            children: [
+              Image(image: AssetImage(store.imagePath), width: 100),
+              const SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(store.name,
+                        style: Theme.of(context).textTheme.headlineSmall),
+                    Text(
+                      "Type: ${store.type}",
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+class StoresHomePage extends StatelessWidget {
+  const StoresHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        StoreCard(
+            store: Store(
+                name: "American Eagle",
+                type: "Clothing",
+                imagePath: "assets/stores-logos/ae.png")),
+        StoreCard(
+            store: Store(
+                name: "H&M",
+                type: "Clothing",
+                imagePath: "assets/stores-logos/hm.png"))
+      ],
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  const ProductCard(
+      {super.key,
+      required this.imagePath,
+      required this.name,
+      required this.price});
+  final String imagePath;
+  final String name;
+  final String price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image(image: AssetImage(imagePath)),
+        const SizedBox(
+          height: 30,
+        ),
+        Text(name, style: Theme.of(context).textTheme.headlineSmall),
+        Text("Price $price", style: Theme.of(context).textTheme.bodyMedium),
+      ],
+    );
+  }
+}
+
+class StorePage extends StatelessWidget {
+  final Store store;
+  const StorePage({super.key, required this.store});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(store.name)),
+      body: Column(children: [
+        Center(
+          child: Image(
+            image: AssetImage(store.imagePath),
+            height: 100,
+          ),
+        )
+      ]),
+    );
+  }
+}
+
+class Store {
+  final String name;
+  final String type;
+  final String imagePath;
+  const Store(
+      {required this.name, required this.type, required this.imagePath});
 }
